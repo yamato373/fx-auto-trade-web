eba3d0fafea7568df1ffd375b85b7f74c77bb101
yamato <yamato@192.168.11.6>
2017-09-22 02:16:14 +0900

    機動シェル回り修正

--- src/main/assembly/start.sh
+++ src/main/assembly/start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+PID_PATH_NAME=/var/run/${project.artifactId}/${project.artifactId}.pid
+
 DIR=`dirname $0`
 cd $DIR
-# java -jar lib/${project.artifactId}-${project.version}.jar $* &
-java ./autotrade-web start
+
+echo "Starting ${project.artifactId} ..."
+if [ ! -f $PID_PATH_NAME ]; then
+	nohup java -jar lib/${project.artifactId}.jar /tmp 2>> /dev/null >> /dev/null &
+	echo $! > $PID_PATH_NAME
+	echo "${project.artifactId} started ..."
+else
+	echo "${project.artifactId} is already running ..."
+fi
